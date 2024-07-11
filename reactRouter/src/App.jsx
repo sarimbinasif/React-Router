be5e02425d093e88510f75d3b9536628eec2e216
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-07-11 23:24:42 +0500

    added Home,About,Contact components

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   
 
   return (
     <>
-      {/* <h1 className='bg-green-600'>React Router</h1> */}
       <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
