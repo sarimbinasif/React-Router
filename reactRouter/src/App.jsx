b7786fe7a5748d160dd5abe8a7c9bf108c9579dc
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-07-11 20:57:46 +0500

    added Header Component

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <h1 className='bg-green-600'>React Router</h1> */}
+      <Header/>
+    </>
+  )
+}
+
+export default App
